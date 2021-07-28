6916ed1adba44fe5ac585cd7619a530864eb00aa
ashley19991014 <1306015953@qq.com>
2021-07-28 18:16:58 +0800

    can add to-dos right now

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from'./components/ToDoItem.vue'
+import uniqueId from 'lodash.uniqueid'
+import ToDoForm from "./components/ToDoForm";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem,
+    ToDoForm
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue Project', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    }
+  },
+  methods: {
+    addToDo(todoLabel) {
+      this.ToDoItems.push({id:uniqueId("todo-"), label: todoLabel, done: false})
+    }
   }
 }
 </script>
@@ -22,5 +47,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style-type: none;
 }
 </style>
---
+++ todo-vue/src/components/ToDoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-bind:id="id" type="checkbox" v-bind:checked="isDone"/>
+    <label v-bind:for="id">{{label}}</label>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      label: { required: true, type: String },
+      done: { default: false, type: Boolean},
+      id: { required: true, type: String }
+    },
+    data() {
+      return {
+        isDone: this.done,
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
